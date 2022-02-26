9ac4136391de84800182a70ff02f3422ca2845d2
Samuel Constantino <samuelicapuidados@gmail.com>
2022-02-26 10:39:06 -0300

    feat: implementa middleware de erro

---
+++ src/middlewares/error.js
@@ -0,0 +1,13 @@
+const { ApiError } = require('../global/api-error');
+
+const error = (err, _req, res, _next) => {
+    console.log(err);
+    if (err instanceof ApiError) {
+        return res.status(err.code).json({
+            message: err.message,
+        });
+    }
+    return res.status(500).json({ message: 'Internal Server Error' });
+};
+
+module.exports = { error };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { taskRouter } = require('./taskRouter');
 const { userRouter } = require('./userRouter');
+const { error } = require('../middlewares');
 
 const router = express.Router({ mergeParams: true });
 
 router.use('/task', taskRouter);
 router.use('/user', userRouter);
+
+router.use(error);
 
 module.exports = { router };
