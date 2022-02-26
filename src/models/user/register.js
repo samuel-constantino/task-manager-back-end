e4ebc9ca1c665a3018de3c0f5b69e5250aa87dc5
Samuel Constantino <samuelicapuidados@gmail.com>
2022-02-26 09:57:05 -0300

    refactor: remove verificação desnecessária de inserção do usuário

--- src/models/user/register.js
+++ src/models/user/register.js
@@ -4,10 +4,7 @@ const { connection } = require('../connection');
 
 const register = async (newUser) => {
     const db = await connection();
-
     const { insertedId: id } = await db.collection('users').insertOne(newUser);
-    if (!id) return null;
-
     const userFound = await findById(id);
     return userFound;
 };
