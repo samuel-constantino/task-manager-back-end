61f33ab540451ca667a58f709a8ce97428ad2d58
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-07 11:12:39 -0300

    refactor: adiciona rescue nas rotas de usuário

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const userController = require('../controllers/user');
 
 const userRouter = express.Router();
 
-userRouter.post('/register', userController.register);
+userRouter.post('/register', rescue(userController.register));
+userRouter.post('/login', rescue(userController.login));
 
 module.exports = { userRouter };
