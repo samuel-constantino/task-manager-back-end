95cff2eebdadbd5682793a06eda720e2085c7574
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-25 07:54:07 -0300

    refactor: modifica tamanho minimo da descrição da tarefa

--- src/services/task/validations/taskValidation.js
+++ src/services/task/validations/taskValidation.js
@@ -4,7 +4,7 @@ const { ApiError: { badRequest } } = require('../../../global/api-error');
 const taskSchema = Joi.object({
   userId: Joi.string().min(24).empty(false).required(),
   name: Joi.string().min(6).empty(false).required(),
-  description: Joi.string().min(10).empty(false).required(),
+  description: Joi.string().min(5).empty(false).required(),
   status: Joi.string()
     .valid('In progress', 'Completed')
     .empty(false)
