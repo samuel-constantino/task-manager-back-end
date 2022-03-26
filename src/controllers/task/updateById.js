8fe4d04e2ac3fb311b82f245522b0598942b6d74
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-26 10:19:48 -0300

    refactor: adiciona propriedade 'priority' na atualização de tarefa

--- src/controllers/task/updateById.js
+++ src/controllers/task/updateById.js
@@ -2,8 +2,8 @@ const taskService = require('../../services/task');
 
 const updateById = async (req, res) => {
     const { id } = req.params;
-    const { name, description, status } = req.body;
-    const updatedTask = await taskService.updateById({ id, name, description, status });
+    const { name, description, status, priority } = req.body;
+    const updatedTask = await taskService.updateById({ id, name, description, status, priority });
     return res.status(200).json(updatedTask);
 };
 
--- src/models/task/updateById.js
+++ src/models/task/updateById.js
@@ -2,12 +2,12 @@ const { ObjectId } = require('mongodb');
 const { connection } = require('../connection');
 
 const updateById = async (task) => {
-    const { id, name, description, status } = task;
+    const { id, name, description, status, priority } = task;
     
     const db = await connection();
 
     const query = { _id: ObjectId(id) };
-    const update = { $set: { name, description, status } };
+    const update = { $set: { name, description, status, priority } };
     const options = { returnDocument: 'after' };
 
     const { value: updatedTask } = await db.collection('tasks').findOneAndUpdate(
