a242645304592da7aea1865090cec5e1eccaaf06
Samuel Constantino <samuelicapuidados@gmail.com>
2022-03-26 10:47:42 -0300

    refactor: adiciona filtro para buscar apenas tarefas não concluídas

--- src/models/task/findByUser.js
+++ src/models/task/findByUser.js
@@ -1,9 +1,11 @@
 const { connection } = require('../connection');
 
 const findByUser = async (userId) => {
-    const db = await connection();
-    const tasksFound = await db.collection('tasks').find({ userId }).toArray();
-    return tasksFound;
+  const db = await connection();
+  const tasksFound = await db.collection('tasks').find(
+    { userId, status: 'In progress' },
+  ).toArray();
+  return tasksFound;
 };
 
 module.exports = { findByUser };
